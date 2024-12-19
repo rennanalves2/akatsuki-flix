f51374f798732f78257928c4270c9fc08d51b66f
Rennan Alves <rennan_areas@hotmail.com>
2024-12-19 18:37:08 -0300

    Create error page

---
+++ src/pages/Error/index.js
@@ -0,0 +1,11 @@
+import "./error.css";
+
+const Error = () => {
+    return (
+        <div className="container-error">
+            <h1>404 Not Found</h1>
+        </div>
+    )
+}
+
+export default Error;
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import Character from "./pages/Character";
 import Header from "./components/Header";
 import Favorites from "./pages/Favorites";
 
+import Error from "./pages/Error";
+
 const RoutesApp = () => {
     return (
         <BrowserRouter>
@@ -13,6 +15,8 @@ const RoutesApp = () => {
                 <Route path="/" element = { <Home /> }/>
                 <Route path="/character/:id" element = { <Character /> }/>
                 <Route path="/favorites" element = { <Favorites /> }/>
+
+                <Route path="*" element = { <Error /> }/>
             </Routes>
         </BrowserRouter>
     )
